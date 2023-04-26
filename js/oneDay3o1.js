560c0b1624a329f102ef82c36ea04a4288e7dfbb
qulexx <giacomo.piperata@lernende.bfo-vs.ch>
2023-04-26 09:35:01 +0200

    sketch1day3 finished

--- js/oneDay3o1.js
+++ js/oneDay3o1.js
@@ -1,44 +1,27 @@
-let agent;
+let position;
+let velocity;
+let size;
 
 function setup() {
   createCanvas(400, 400);
-  agent = new Agent(width/2, height/2);
+  position = createVector(width/2, height/2); 
+  velocity = createVector(random(-10, 10), random(-10, 10)); 
+  size = 20; 
 }
 
 function draw() {
   background(220);
-  agent.update();
-  agent.display();
-}
+  fill(255, 0, 155); 
+  ellipse(position.x, position.y, size, size); 
 
-class Agent {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector();
-    this.acc = createVector();
-    this.maxSpeed = 3;
-    this.maxForce = 0.1;
-    this.color = color(random(255), random(255), random(255));
+  position.add(velocity); 
+  
+  //If Ball hit left or right edge of canvas then multiply x velocity by -1 (reverse )
+  if(position.x < 0 || position.x > width) {
+    velocity.x *= -1;
   }
-
-  update() {
-    let desired = p5.Vector.random2D();
-    desired.mult(this.maxSpeed);
-    let steer = p5.Vector.sub(desired, this.vel);
-    steer.limit(this.maxForce);
-    this.acc.add(steer);
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxSpeed);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  display() {
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.vel.heading());
-    fill(this.color);
-    triangle(-10, -5, -10, 5, 10, 0);
-    pop();
+  
+  if(position.y < 0 || position.y > height) {
+    velocity.y *= -1;
   }
 }
